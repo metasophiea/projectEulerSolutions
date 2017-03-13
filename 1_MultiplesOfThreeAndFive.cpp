f02c677278b433784ee9ce21f9074a8ef2adecbd
metasophiea <metasophiea@gmail.com>
2017-03-13 15:08:48 +0000

    Update 1_MultiplesOfThreeAndFive.cpp

--- 1_MultiplesOfThreeAndFive.cpp
+++ 1_MultiplesOfThreeAndFive.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
+unsigned int function(){
+	unsigned int limit = 1000;
+	unsigned int sum = 0;
+	unsigned int multiple_a = 3, multiple_b = 5;
+
+	unsigned int temp = 0, count = 0;
+	while(temp < limit){
+		sum += temp; count++;
+		temp = count*multiple_a;	
+	}
+	
+	temp = 0, count = 0;
+	while(temp < limit){
+		count++;
+		if(count % multiple_a != 0){
+			sum += temp; 
+			temp = count*multiple_b;
+		}
+	}
+	
+	return sum;
+}
+
 int main(int argumentCount, char *arguments[]){
 	std::cout << "Hello" << std::endl;
+	std::cout << function() << std::endl;
 	return 0;
 }
